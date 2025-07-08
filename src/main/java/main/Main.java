12528578a03a05b3662600b931efd56db9c34e4c
YoanaYosifova <y.yosifova@student.tue.nl>
2025-07-08 20:24:31 +0300

    Remove excess code

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,15 +1,12 @@
 package main;
 
-import javax.swing.ImageIcon;
+import java.awt.Color;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.SwingUtilities;
 
 import gui.GamePanel;
-
-// import java.awt.*;
-// import java.awt.event.ActionEvent;
-// import java.awt.event.ActionListener;
 
 /**
  * Main class for the game application.
@@ -26,16 +23,30 @@ public class Main extends JFrame {
             
             JFrame frame = new JFrame("My Monthly Cycle");
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        
-            GamePanel gamePanel = new GamePanel();
-            frame.add(gamePanel);
-             
-            //pack() should be called before setting the location and visibility
-            frame.pack(); // Pack the frame to fit the preferred size of the game panel
-        
-            frame.setLocationRelativeTo(null); // Center the window on the screen
-            frame.setVisible(true); // Make the frame visible
-            gamePanel.startGameThread(); // Start the game loop after the panel is set up and visible.
+            frame.setBackground(Color.BLACK);
+
+            JLabel gameDescription = new JLabel("Welcome to 'My Monthly Cycle' education game! \n Press the 'START' button to begin");
+            gameDescription.setHorizontalAlignment(JLabel.CENTER);
+            frame.add(gameDescription);
+
+            JButton startButton = new JButton("START");
+            startButton.addActionListener(e -> {
+                // Remove the description label and start button when the game starts
+                frame.remove(gameDescription);
+                frame.remove(startButton);
+                frame.revalidate(); // Refresh the frame to reflect changes
+                frame.repaint(); // Repaint the frame to update the display
+
+                GamePanel gamePanel = new GamePanel();
+                frame.add(gamePanel);
+                
+                //pack() should be called before setting the location and visibility
+                frame.pack(); // Pack the frame to fit the preferred size of the game panel
+            
+                frame.setLocationRelativeTo(null); // Center the window on the screen
+                frame.setVisible(true); // Make the frame visible
+                gamePanel.startGameThread(); // Start the game loop after the panel is set up and visible.
+            });
         });
     }
 }
